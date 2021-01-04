ac1d16e208fd0d27c4322f47a4e21f4a247dd37a
Joshua Eckert <eckertjoshua@aol.de>
2021-01-04 10:16:43 +0100

    variables.py

---
+++ variables.py
@@ -0,0 +1,9 @@
+
+character_name = "John"
+character_age = "35"
+print("There was once a man named " + character_name + ", ")
+print("he was " + character_age + " years old. ")
+
+character_name = "Mike"
+print("He really liked the name " + character_name + ", ")
+print("bit didn't like being " + character_age + ", ")
